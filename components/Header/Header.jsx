76b3dc6916ce68e8eab22ee56ee6d2202a8597a7
Wadson Lucas <wlsp@Wadsons-MacBook-Pro.local>
2021-12-19 22:29:12 +0100

    fix/ img path

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
             bg-slate-50 shadow-md p-5 md:px-10 z-50" >
                   <div className="relative flex items-center h-10
                   cursor-pointer my-auto">
-                        <Image src={'/images/Airbnb_Logo_Bélo.svg.png'}
+                        <Image src={'/images/Airbnb_Logo_Bélo.png'}
                               layout={"fill"}
                               objectFit={"contain"} objectPosition={"left"}
                         />
